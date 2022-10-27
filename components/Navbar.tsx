0c1761e90f776542411db199f1d53f2dcbc209ad
Veter v Golove <koshelevworkmail@gmail.com>
2022-10-27 14:26:58 -0400

    Commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,15 @@ import { GoogleLogout, GoogleLogin } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
-import Logo from "../utils/tiktik-logo.png";
+import Logo from "../utils/blackbox.png";
 
 const Navbar = () => {
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
-                <div className="w-[100px] md:w-[130px]">
+                <div className="w-[130px] md:w-[130px]">
+                    {" "}
+                    {/* md:w-[130px] */}
                     <Image
                         className="cursor-pointer"
                         src={Logo}
