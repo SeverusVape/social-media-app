9fbdc2563484e265d18d73bdac20a65e786cc7e9
Veter v Golove <koshelevworkmail@gmail.com>
2022-10-28 18:46:07 -0400

    Google oauth token

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogout, GoogleLogin } from "react-google-login";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/blackbox.png";
 
 const Navbar = () => {
+    const user = false;
+
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
@@ -23,6 +25,21 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
+            <div>SEARCH</div>
+            <div>
+                {user ? (
+                    <div>Logged in</div>
+                ) : (
+                    <GoogleLogin
+                        onSuccess={(response) => {
+                            console.log(response);
+                        }}
+                        onError={() => {
+                            console.log("ERROR");
+                        }}
+                    />
+                )}
+            </div>
         </div>
     );
 };
