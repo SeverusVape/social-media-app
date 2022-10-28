ebd61a89e8b4e59cb620d7d1acb01390fbafa096
Veter v Golove <koshelevworkmail@gmail.com>
2022-10-28 19:41:45 -0400

    Google log in user

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,11 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/blackbox.png";
+import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-    const user = false;
+    const { userProfile, addUser } = useAuthStore();
 
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -27,12 +29,12 @@ const Navbar = () => {
             </Link>
             <div>SEARCH</div>
             <div>
-                {user ? (
-                    <div>Logged in</div>
+                {userProfile ? (
+                    <div>{userProfile.userName}</div>
                 ) : (
                     <GoogleLogin
                         onSuccess={(response) => {
-                            console.log(response);
+                            createOrGetUser(response, addUser);
                         }}
                         onError={() => {
                             console.log("ERROR");
