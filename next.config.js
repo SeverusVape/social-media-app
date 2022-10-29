a32bb826880fed9494d0c4bfbbe49290f77b9804
Veter v Golove <koshelevworkmail@gmail.com>
2022-10-28 20:05:12 -0400

    LogIn/LogOut functionality with react-oauth/google

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["cdna.artstation.com"],
+        domains: ["cdna.artstation.com", "lh3.googleusercontent.com"],
     },
 };
 
